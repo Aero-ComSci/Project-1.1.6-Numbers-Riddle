f8783012753c8617d1761dc81cc366e575c3e9ef
Rudra Goel <rudragoel10@gmail.com>
2026-09-08 18:06:14 -0700

    How we will use GIT

--- main.java
+++ main.java
@@ -1,3 +1,14 @@
+/*
+  GIT WORKFLOW:
+  1. git pull       → Get the latest code from GitHub.
+  2. Write your code and save it.
+  3. git add .      → Prepare your changes.
+  4. git commit -m "..." → Save your changes with a msg in the quotes.
+  5. git push       → Send your changes to GitHub.
+ 
+ Pull before starting work and push when you finish.
+ */
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
