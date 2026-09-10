6bdb45a86355aca05a3ef0c648e1832cc350c47b
Sidhant Ramasayam <ramasayam0819@dusd.local>
2026-09-10 08:47:15 -0700

    hi

--- always_three.java
+++ always_three.java
@@ -2,10 +2,13 @@ class always_three {
     public static void main(String[] args) {
         int x = 1290819;
         int temp = x;
-        
+        System.out.println(x);
         x *= 2;
+        System.out.println(x);
         x += 6;
+        System.out.println(x);
         x /= 2;
+        System.out.println(x);
         x -= temp;
         System.out.println(x);
     }
